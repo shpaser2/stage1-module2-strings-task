fcb79b5c6593d52bf75cef16acecb1158a12fc94
Siarhei_Shpakouski <siarhei_shpakouski@epam.com>
2023-05-13 19:28:18 +0400

    Revert "done"
    This reverts commit 843449e0398d72c1d21c6f788fc7d54d5c955d44.

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,10 +1,7 @@
 package com.epam.mjc;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class StringSplitter {
 
@@ -16,12 +13,6 @@ public class StringSplitter {
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        String delimiterRegex = delimiters.stream()
-                .map(Pattern::quote) // escape any special regex characters
-                .collect(Collectors.joining("|")); // join delimiters with OR operator
-
-        return Arrays.stream(source.split(delimiterRegex))
-                .filter(s -> !s.isEmpty()) // filter out empty strings
-                .collect(Collectors.toList());
+        throw new UnsupportedOperationException("You should implement this method.");
     }
 }
